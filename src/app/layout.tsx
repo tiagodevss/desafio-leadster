36e20c6b6e1e92277cabbd7014821416d28e692d
TiagoSaveway <tiagosantos@saveway.com.br>
2023-06-27 10:13:07 -0300

    Primeiro commit + adição das fontes e estilos globais base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import '../styles/globals.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +11,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body style={{fontFamily: "PlusJakartaSans-Regular"}}>{children}</body>
     </html>
   )
 }
